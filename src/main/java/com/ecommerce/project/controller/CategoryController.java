180a3de3472efd79c22ed9883f1bcabc4103d8e8
developerdevanshh <vermadev8749@gmail.com>
2026-07-06 21:22:06 +0530

    Ecommerce Project

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,4 +1,13 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.model.Category;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
 public class CategoryController {
+    private List<Category> categories = new ArrayList<>();
+
+
 }
