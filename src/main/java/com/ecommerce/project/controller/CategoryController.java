af02fa6d17eba82d3477699887ba615990def945
developerdevanshh <vermadev8749@gmail.com>
2026-07-07 18:03:15 +0530

    first commit

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,17 +2,30 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
 @RestController
 public class CategoryController {
-    private List<Category> categories = new ArrayList<>();
+//    private final List<Category> categories = new ArrayList<>();
 
-    @GetMapping("/api/public/categories")
-    public List<Category> getAllCategories(){
-        return categories;
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
     }
+
+//    @GetMapping("/api/public/categories")
+//    public List<Category> getAllCategories(){
+//        return categories;
+//    }
+//
+//    @PostMapping("/api/public/categories")
+//    public String createCategory(@RequestBody Category category){
+//        categories.add(category);
+//        return "Category created successfully";
+//    }
 
 }
