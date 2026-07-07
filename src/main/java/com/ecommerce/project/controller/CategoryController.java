e339016bbba43c3b431143ff390adfe913a0e2dc
developerdevanshh <vermadev8749@gmail.com>
2026-07-07 17:29:31 +0530

    Ecommerce Project

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class CategoryController {
     private List<Category> categories = new ArrayList<>();
 
-    @GetMapping("api/public/categories")
+    @GetMapping("/api/public/categories")
     public List<Category> getAllCategories(){
         return categories;
     }
