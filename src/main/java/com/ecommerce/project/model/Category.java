268437b0edd5f95cd089b2ff3e159b7a8cf429c9
developerdevanshh <vermadev8749@gmail.com>
2026-07-15 19:37:25 +0530

    Category Updated

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -20,13 +20,13 @@ public class Category {
     public Category() {
     }
 
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
+//    public String getCategoryName() {
+//        return categoryName;
+//    }
+//
+//    public void setCategoryName(String categoryName) {
+//        this.categoryName = categoryName;
+//    }
 
     public Long getCategoryId() {
         return categoryId;
